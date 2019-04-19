5122dd8d6a75abeea88f06770c0b3cb8f44ccd2d
EmmanuelYupit <emmanuelyupit08@gmail.com>
2019-04-19 16:58:18 -0500

    create user

--- api/api/controllers/UserController.js
+++ api/api/controllers/UserController.js
@@ -9,7 +9,25 @@ const create = async (req, res) => {
   try {
     const { name, lastName, email, password, type } = req.allParams();
     const password = sails.helpers.encryptPassword(passString);
-  } catch (error) {}
+    const { id } = await Usuarios.create({
+      name,
+      lastName,
+      type,
+      email,
+      password,
+    });
+    const user = await User.findOne({ id });
+    const token = sails.helpers.generateToken.with({
+      id: user.id,
+      email: user.email,
+      login: moment().format(),
+      key: sails.config.session.secret,
+    });
+    delete user.password;
+    res.created({ user, token });
+  } catch (err) {
+    res.handle(err);
+  }
 };
 
 module.exports = {
