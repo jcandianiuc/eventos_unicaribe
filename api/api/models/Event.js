aafcb4d2394480d94f2a0b7c72c81e489d7c56df
EmmanuelYupit <emmanuelyupit08@gmail.com>
2019-04-22 21:06:19 -0500

    event api added

---
+++ api/api/models/Event.js
@@ -0,0 +1,47 @@
+/**
+ * Event.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    name: {
+      type: 'string',
+    },
+    date: {
+      type: 'datetime',
+    },
+    startTime: {
+      type: 'datetime',
+    },
+    endTime: {
+      type: 'datetime',
+    },
+    cost: {
+      type: 'float',
+    },
+    type: {
+      type: 'string',
+      enum: ['conference', 'play'],
+    },
+    Place: {
+      model: 'Place',
+    },
+    Attendants: {
+      collection: 'Assistant',
+      via: 'Events',
+    },
+    Talker: {
+      model: 'Talker',
+    },
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+};
